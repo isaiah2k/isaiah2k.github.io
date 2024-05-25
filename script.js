b494bd512cc8a950f97de26dd47df52b03de422c
Isaiah <zaymgmt707@gmail.com>
2024-05-25 15:34:18 -0700

    updates index.html style.css

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This content was made by ZAY";
 document.querySelector("body").appendChild(h2);
+function toggleSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
